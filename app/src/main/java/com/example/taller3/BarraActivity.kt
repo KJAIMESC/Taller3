e396890640820ada1a906ee7fe5fddfbc8e91941
Alanis Forero <104458684+KJAIMESC@users.noreply.github.com>
2024-05-09 13:58:44 -0500

    barra ya esta
    Falta funcionalidad

---
+++ app/src/main/java/com/example/taller3/BarraActivity.kt
@@ -0,0 +1,59 @@
+package com.example.taller3
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.PopupMenu
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
+
+class BarraActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_barra)
+
+        val toolbar: MaterialToolbar = findViewById(R.id.topAppBar)
+        setSupportActionBar(toolbar)
+
+        toolbar.setNavigationOnClickListener {
+            showNavigationMenu(it)
+        }
+
+        toolbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.salir_perfil -> {
+                    // Cuando se selecciona "Salir del perfil", inicia MainActivity
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                    true
+                }
+                R.id.perfil -> {
+                    // Manejar clic en el icono de búsqueda
+                    true
+                }
+                R.id.more -> {
+                    // Manejar más elementos en el menú desbordamiento
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun showNavigationMenu(view: android.view.View) {
+        val popup = PopupMenu(this, view)
+        popup.menuInflater.inflate(R.menu.navigation_menu, popup.menu)
+        popup.setOnMenuItemClickListener { menuItem: MenuItem ->
+            when (menuItem.itemId) {
+                R.id.set_as_available -> {
+                    true
+                }
+                // Agrega más casos si hay más ítems
+                else -> false
+            }
+        }
+        popup.show()
+    }
+}
+
