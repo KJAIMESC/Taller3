f09553467d2d7a163f1f8cee20d4454f8200dc25
Alanis Forero <116515549+Alan1s@users.noreply.github.com>
2024-05-12 01:26:15 -0500

    agregar ciertas dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,10 +38,31 @@ android {
 dependencies {
     val fragment_version = "1.7.0"
 
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("junit:junit:4.13.2")
+    implementation("androidx.test.ext:junit:1.1.5")
+    implementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("com.google.dagger:hilt-android:2.40")
+    implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation("com.github.bumptech.glide:glide:4.13.2")
+    implementation("com.google.firebase:firebase-firestore:17.1.2")
+    implementation("com.google.firebase:firebase-auth:19.3.2")
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-storage-ktx:21.0.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.4.1")
+
+    annotationProcessor("com.github.bumptech.glide:compiler:4.13.2")
+
     //mapa
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("androidx.fragment:fragment-ktx:$fragment_version")
-    implementation("androidx.appcompat:appcompat:1.3.0")
     implementation("androidx.fragment:fragment:1.3.6")
     implementation("com.google.android.gms:play-services-location:18.0.0")
     implementation("de.hdodenhof:circleimageview:3.1.0")
@@ -50,8 +71,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.play.services.location)
-    implementation(libs.volley)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
