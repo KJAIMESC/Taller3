68e97dc47a42a5c908611f6b9ea3efe5c681618b
KJAIMESC <104458684+KJAIMESC@users.noreply.github.com>
2024-05-14 22:28:51 -0500

    cosita de logout

--- app/src/main/java/com/example/taller3/BarraActivity.kt
+++ app/src/main/java/com/example/taller3/BarraActivity.kt
@@ -43,9 +43,10 @@ open class BarraActivity : AppCompatActivity() {
         toolbar.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.salir_perfil -> {
+                    auth.signOut()
                     val intent = Intent(this, MainActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
                     startActivity(intent)
-                    finish()
                     true
                 }
                 R.id.perfil -> {
